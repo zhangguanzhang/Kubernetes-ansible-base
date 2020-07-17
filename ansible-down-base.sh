a5f15f6be148b9a8c21f638b4a16aea7f559ff8a
zhangguanzhang <zhangguanzhang@qq.com>
2020-07-17 16:47:33 +0800

    update the repo

--- ansible-down-base.sh
+++ ansible-down-base.sh
@@ -2,16 +2,16 @@
 
 function down_kube(){
     [ ! -f kubernetes-server-linux-amd64.tar.gz ] && {
-        docker pull zhangguanzhang/k8s_bin:$KUBE_VERSION-full
-        docker run --rm -d --name kube zhangguanzhang/k8s_bin:$KUBE_VERSION-full sleep 10
+        docker pull registry.aliyuncs.com/zhangguanzhang/k8s_bin:$KUBE_VERSION-full
+        docker run --rm -d --name kube registry.aliyuncs.com/zhangguanzhang/k8s_bin:$KUBE_VERSION-full sleep 10
         docker cp kube:/kubernetes-server-linux-amd64.tar.gz .
         tar -zxvf kubernetes-server-linux-amd64.tar.gz  --strip-components=3 -C /usr/local/bin kubernetes/server/bin/kube{let,ctl,-apiserver,-controller-manager,-scheduler,-proxy}
     } || :
 }
 
 function down_etcd(){
-    docker pull zhangguanzhang/quay.io.coreos.etcd:$ETCD_version
-    docker run --rm -d --name etcd zhangguanzhang/quay.io.coreos.etcd:$ETCD_version sleep 10
+    docker pull registry.aliyuncs.com/k8sxio/etcd:$ETCD_version
+    docker run --rm -d --name etcd registry.aliyuncs.com/k8sxio/etcd:$ETCD_version sleep 10
     docker cp etcd:/usr/local/bin/etcd /usr/local/bin
     docker cp etcd:/usr/local/bin/etcdctl /usr/local/bin
 }
@@ -31,8 +31,8 @@ function down_flanneld(){
 function down_cni(){
     [ ! -f cni-plugins-linux-amd64-${CNI_VERSION}.tgz ] && \
 #    wget "${CNI_URL}/${CNI_VERSION}/cni-plugins-linux-amd64-${CNI_VERSION}.tgz" 
-    docker pull zhangguanzhang/cni-plugins:${OS}-${ARCH}-${CNI_VERSION}
-    docker run -d --rm --name cni zhangguanzhang/cni-plugins:${OS}-${ARCH}-${CNI_VERSION} sleep 10
+    docker pull registry.aliyuncs.com/zhangguanzhang/cni-plugins:${OS}-${ARCH}-${CNI_VERSION}
+    docker run -d --rm --name cni registry.aliyuncs.com/zhangguanzhang/cni-plugins:${OS}-${ARCH}-${CNI_VERSION} sleep 10
     docker cp cni:/cni-plugins-${OS}-${ARCH}-${CNI_VERSION}.tgz .
 }
 
